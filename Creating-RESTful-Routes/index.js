97f699134448e3bb0449688ff5a3a71c3384f690
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-10-24 23:20:47 +0300

    Patch route using method-override for resource

--- Creating-RESTful-Routes/index.js
+++ Creating-RESTful-Routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const {v4 : getID} = require("uuid");
+const {v4 : getID} = require('uuid');
+const methodOverride = require('method-override');
 
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 const comments = [
     {
@@ -44,6 +46,20 @@ app.get("/comments/:id", (req,res) => {
     const {id} = req.params;
     const comment = comments.find( (comment) => comment.id === id);
     res.render("comments/show", {comment});
+});
+
+app.patch("/comments/:id", (req,res) => {
+    const {id} = req.params;
+    const {updatedComment} = req.body
+    const foundComment = comments.find( (comment) => comment.id === id);
+    foundComment.comment = updatedComment;
+    res.redirect("/comments");
+});
+
+app.get("/comments/:id/edit", (req,res) => {
+    const {id} = req.params;
+    const comment = comments.find( (comment) => comment.id === id);
+    res.render("comments/edit",{comment});
 });
 
 app.listen(3000,() => {
