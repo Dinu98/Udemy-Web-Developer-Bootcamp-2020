de47b9dbaf53ee594d44edb15cdf3ad44e140fed
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-10-30 23:19:31 +0200

    Simple example of protecting a route

--- Middleware/index.js
+++ Middleware/index.js
@@ -2,10 +2,23 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const verifyPassword = (req,res,next) =>{
+    const { password } = req.query
+    if(password === "Pass"){
+        return next();
+    } else {
+        res.send("You need a password to access this route");
+    }
+}
+
 app.use(morgan("dev"));
 
 app.get("/", (req,res) => {
     res.send("It works");
+});
+
+app.get("/database", verifyPassword, (req,res) =>{
+    res.send("You can now access our database");
 });
 
 app.listen(3000, () =>{
