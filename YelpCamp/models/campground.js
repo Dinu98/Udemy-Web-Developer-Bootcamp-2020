3b00f6ae71b8b649a880d6efa902d9f45a46310c
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-11-02 23:19:10 +0200

    Added review model

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
-const campgroundSchema = new mongoose.Schema({
+const { Schema } = mongoose;
+
+const campgroundSchema = new Schema({
     name: String,
     price: Number,
     description: String,
     location: String,
-    image: String
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
---
+++ YelpCamp/models/review.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema ({
+    text: String,
+    rating: Number
+});
+
+const Review = mongoose.model("Review",reviewSchema);
+
+module.exports = Review;
