846b349a6246595b33f69c0c399fce167a9d3883
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-11-02 16:39:22 +0200

    Show route for farms

--- 9.Mongo-Relantionships-With-Express/index.js
+++ 9.Mongo-Relantionships-With-Express/index.js
@@ -38,6 +38,11 @@ app.get("/farms", async (req,res) => {
     res.render("farms/index", { farms });
 });
 
+app.get("/farms/:id", async (req,res) => {
+    const farm = await Farm.findById(req.params.id);
+    res.render("farms/show", { farm });
+});
+
 app.get("/products", async (req,res) => {
     let { category } = req.query;
     let products = [];
