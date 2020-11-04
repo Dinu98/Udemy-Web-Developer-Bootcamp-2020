3b2d83b4b4484cc1f26df3ef85ddf2ea5761fab6
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-11-05 01:22:36 +0200

    More session

--- L.Sessions-In-Express/index.js
+++ L.Sessions-In-Express/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 
-app.use(session({secret: "secret"}));
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 //route that increments a variable every time a user views this page
@@ -13,6 +17,17 @@ app.get("/", (req,res) => {
         req.session.count = 1;
     }
     res.send(`You have viewed this page ${req.session.count} times`);
+});
+
+app.get("/register", (req,res) => {
+    const { username = "Unknow"} = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+});
+
+app.get("/greet", (req,res) => {
+    const { username } = req.session;
+    res.send(`Welcome back ${username}`);
 });
 
 app.listen(3000, () => {
