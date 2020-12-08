03c56f94f9f95246a1b24a3df94d5f9802a49a72
Dinescu Alexandru-Catalin <alexdinescu888@gmail.com>
2020-12-08 23:34:42 +0200

    basic prevention against XSS

--- YelpCamp/validateSchemas.js
+++ YelpCamp/validateSchemas.js
@@ -1,10 +1,33 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 campgroundsSchema = Joi.object({
     campground: Joi.object({
-        name: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required(),
+        name: Joi.string().required().escapeHTML(),
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML(),
         // image: Joi.string().required(),
         price: Joi.number().min(0).required()
 
@@ -14,7 +37,7 @@ campgroundsSchema = Joi.object({
 
 reviewSchema = Joi.object({
     review: Joi.object({
-        text: Joi.string().required(),
+        text: Joi.string().required().escapeHTML(),
         rating: Joi.number().min(1).max(5).required()
     }).required()
 })
